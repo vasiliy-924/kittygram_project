340fbc58f29f2eb972902e9b3f522114310a2db8
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-06-01 13:44:58 +0300

    add cat_detail

--- cats/views.py
+++ cats/views.py
@@ -16,4 +16,32 @@ def cat_list(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     cats = Cat.objects.all()
     serializer = CatSerializer(cats, many=True)
-    return Response(serializer.data)
+    return Response(serializer.data) 
+
+
+@api_view(['GET','PUT', 'PATCH', 'DELETE'])
+def cat_detail(request, pk):
+    try:
+        cat = Cat.objects.get(id=pk)
+    except Cat.DoesNotExist:
+        return Response(
+            {'error': 'Cat not found'},
+            status=status.HTTP_404_NOT_FOUND
+        )
+
+    if request.method == 'GET':
+        serializer = CatSerializer(cat)
+        return Response(serializer.data)
+    
+    elif request.method == 'PUT' or request.method == 'PATCH':
+        serializer = CatSerializer(cat, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    elif request.method == 'DELETE':
+        cat.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+    
+    
